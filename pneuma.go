9fcecd4c7bbaaeb6e7a20927624f1448a8bbe605
Julius Stopforth <jjstopforth@gmail.com>
2021-01-13 15:49:59 +0200

    Add basic TUI with tcell
    Added basic output using https://github.com/gdamore/tcell. Most crucial
    functions putRune and putStr needed to write text to screen.

--- pneuma.go
+++ pneuma.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	//"bufio"
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
@@ -9,8 +8,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
-	//"path"
-	//"strconv"
+	"path"
 	"strings"
 )
 
@@ -104,46 +102,47 @@ func startEditor(path string) {
 }
 
 func main() {
-	/*home, err := os.UserHomeDir()
+	home, err := os.UserHomeDir()
 	configPath := path.Join(home, ".config", "pneuma.json")
 	check(err)
 	os.Chdir(home)
 	cwd, err := os.Getwd()
 	check(err)
-	fmt.Println("Current working dir: ", cwd)
-	fmt.Println("Config path: ", configPath)
-	fmt.Println("Sites from config:")
-	fmt.Println("------------------")
+	screen := initScreen()
 	sites := readConfig(configPath)
-	if len(sites) > 0 {
-		for i, s := range sites {
-			fmt.Printf("%d) %s\n", i+1, s.Name)
-			fmt.Printf("Path: %s\n\n", s.Path)
-		}
-	}
-	fmt.Printf("Please choose a site (default=1): ")
-	inputReader := bufio.NewReader(os.Stdin)
-	inputString, err := inputReader.ReadString('\n')
-	check(err)
-	choice, err := strconv.Atoi(inputString[:len(inputString)-1])
-	choice--
-	check(err)
+	line := 0
+	putStr(0, line, screen, fmt.Sprintf("Current working dir: %s", cwd))
+	line++
+	putStr(0, line, screen, fmt.Sprintf("Config path: %s", configPath))
+	line++
+	putStr(0, line, screen, fmt.Sprintf("Sites from config:"))
+	line++
+	showSiteList(0, line, screen, sites)
+	line += len(sites)
+	putStr(0, line, screen, fmt.Sprintf("Please choose a site (default=1): "))
+	choice := 0
+	var posts []Post
 	if choice < len(sites) {
 		os.Chdir(sites[choice].Path)
 		//siteDir, err := os.Getwd()
 		check(err)
 		//siteConfig := hugoConfig()
 		//fmt.Println(siteConfig)
-		posts := hugoGetPosts()
-		printPosts(posts)
+		posts = hugoGetPosts()
+		//printPosts(posts)
 		//startEditor(posts[0].Path)
-	}*/
-	screen, err := tcell.NewScreen()
-	check(err)
-	err = screen.Init()
-	check(err)
-	screen.Clear()
+	}
+	var postList []string
+	for _, p := range posts {
+		indicator := ""
+		if p.Draft {
+			indicator = "*"
+		}
+		postList = append(postList, fmt.Sprintf("%s%s\n", indicator, p.Title))
+	}
+
 	for {
+		printList(0, line+1, screen, postList)
 		screen.Sync()
 		switch ev := screen.PollEvent().(type) {
 		case *tcell.EventKey:
@@ -156,4 +155,43 @@ func main() {
 		}
 
 	}
+}
+
+func putRune(x, y int, s tcell.Screen, r rune) {
+	s.SetContent(x, y, r, []rune{}, tcell.StyleDefault)
+}
+
+func putStr(x, y int, s tcell.Screen, str string) {
+	for _, c := range str {
+		putRune(x, y, s, rune(c))
+		x++
+	}
+}
+
+func printList(x, y int, s tcell.Screen, list []string) {
+	for num, item := range list {
+		outStr := fmt.Sprintf("%d) %s", num+1, item)
+		putStr(x, y, s, outStr)
+		y++
+	}
+}
+
+func initScreen() tcell.Screen {
+	screen, err := tcell.NewScreen()
+	check(err)
+	err = screen.Init()
+	check(err)
+	screen.Clear()
+	return screen
+}
+
+func showSiteList(x, y int, screen tcell.Screen, sites []Site) {
+	line := y
+	var siteList []string
+	if len(sites) > 0 {
+		for _, s := range sites {
+			siteList = append(siteList, s.Name)
+		}
+	}
+	printList(0, line, screen, siteList)
 }
