e2a9b47b2d7970af0337082e7f7f7ab6f45e93e6
Julius Stopforth <jjstopforth@gmail.com>
2021-01-14 14:00:00 +0200

    Add interactive post edit selection
    The posts from a given site are now loaded and can be selected. On
    selecting a post, it opens up the editor (currently vim).

--- pneuma.go
+++ pneuma.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strconv"
 )
 
 type Site struct {
@@ -76,15 +77,58 @@ func main() {
 	ui.PutStr(fmt.Sprintf("Sites from config:"))
 	var sitesList []string
 	for _, site := range sites {
-		sitesList = append(sitesList, site.Name)
+		sitesList = append(sitesList, fmt.Sprintf("%s (%s)", site.Name, site.Path))
 	}
 	ui.MoveCursor(0, 3)
 	ui.PrintList(sitesList)
 	ui.MoveCursor(0, 3+len(sitesList))
-	ui.PutStr(fmt.Sprintf("Please choose a site (default=1): "))
+	prompt := "Please choose a site (default=1): "
+	ui.PutStr(prompt)
+	ui.MoveCursor(len(prompt), 3+len(sitesList))
+
+	siteSelect := getSelection(ui, len(sitesList))
+
+	ui.Reset()
+	site := hugo.Load(sites[siteSelect].Path)
+	posts := site.Posts
+	var postList []string
+	for _, post := range posts {
+		indicator := ""
+		if post.Draft {
+			indicator = "*"
+		}
+		postList = append(postList, fmt.Sprintf("%s%s", indicator, post.Title))
+	}
+
+	ui.PutStr("Posts from the blog:")
+	ui.MoveCursor(0, 1)
+	ui.PrintList(postList)
+
+	ui.MoveCursor(0, 2+len(postList))
+	prompt = "Select a post to edit: "
+	ui.PutStr(prompt)
+	ui.MoveCursor(len(prompt), 2+len(postList))
+
+	postNum := getSelection(ui, len(postList))
+	startEditor(path.Join(sites[siteSelect].Path, posts[postNum].Path))
 
 	for {
 		ui.Tick()
 	}
 
+}
+
+func getSelection(ui libui.PneumaUI, max int) int {
+	selection := 0
+	input := ui.WaitForInput()
+	choice, err := strconv.Atoi(input)
+	if err == nil {
+		choice--
+		if choice > 0 && choice <= max {
+			selection = choice
+		} else {
+			check(err)
+		}
+	}
+	return selection
 }
