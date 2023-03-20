3644cde9b9577634f783e7be8f350d71cfa21b50
Julius Stopforth <jjstopforth@gmail.com>
2023-03-20 17:42:54 +0200

    add: skeleton git command interface
    The skeleton git interface can only fetch and check for changes in the
    git repo within a blog's dir. It might be pulled completely for a while.

--- internal/git/git.go
+++ internal/git/git.go
@@ -1,8 +1,18 @@
 package git
 
 import (
+	"fmt"
+	"log"
 	"os/exec"
+	"time"
 )
+
+type RepoStatus struct {
+	ModifiedFiles  []string
+	UntrackedFiles []string
+	CanPull        bool
+	CanPush        bool
+}
 
 // Checks whether or not the path is a git repo
 func IsRepo() bool {
@@ -31,5 +41,29 @@ func Update() {
 	if err != nil {
 		panic(err)
 	}
+
+}
+
+// Get a list of all of the files that have been added (untracked) or modified
+// as well as if there are any pulls that can be made
+// Returns a RepoStatus struct
+func Status() string {
+	cmd := exec.Command("git", "status", "--porcelain")
+	result, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	return string(result)
+}
+
+func Sync() {
+	status := Status()
+	if len(status) > 0 {
+		//cmd := exec.Command("git", "add", "--all")
+		msg := fmt.Sprintf("hydra sync at %d\n\n%s", time.Now().Unix(), status)
+		log.Println(msg)
+	}
+	// if len(status.ModifiedFiles) > 0 || len(status.UntrackedFiles) > 0 {
+	// }
 
 }
